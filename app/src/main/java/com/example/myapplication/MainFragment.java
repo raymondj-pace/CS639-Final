f1dbb1844fc7ab21ee33c23585ea31b6881113d3
rmansuri <mansurirushda7@gmail.com>
2023-04-22 10:56:27 -0400

    Updated

--- app/src/main/java/com/example/myapplication/MainFragment.java
+++ app/src/main/java/com/example/myapplication/MainFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.myapplication.databinding.FragmentMainBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,30 +25,54 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import com.hudomju.swipe.SwipeToDismissTouchListener;
-import com.hudomju.swipe.adapter.ListViewAdapter;
-
 public class MainFragment extends Fragment {
 
     private static final String TAG = "MainFragment";
     DatabaseReference myRef;
     private long snapShotRowCount = 0;
     private ListView simpleList;
-    private View view;
     private ArrayList<MoneyTransaction> transactions;
+
+    private ListViewAdapter adapter;
+
+    private Context mContext;
+
+    private FragmentMainBinding binding;
+
+    // Save the context here onAttach - it's not guaranteed to be non-null onViewCreated !!!
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        // Code here to populate list
-        Log.d(TAG, "End Fragment Main");
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        //view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = binding.getRoot();
 
         simpleList = view.findViewById(R.id.simpleListView);
-        transactions = new ArrayList<MoneyTransaction>();
+        transactions = new ArrayList<>();
+
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button2).setOnClickListener(view1 -> {
+            MainFragmentDirections.ActionMainToAddFunds action = MainFragmentDirections.actionMainToAddFunds(0);
+            NavHostFragment.findNavController(MainFragment.this).navigate(action);
+        });
+
+        view.findViewById(R.id.button3).setOnClickListener(view12 -> {
+            MainFragmentDirections.ActionMainToDeduction action = MainFragmentDirections.actionMainToDeduction(0);
+            NavHostFragment.findNavController(MainFragment.this).navigate(action);
+        });
 
         /*
          *  Read Firebase contents
@@ -69,7 +95,9 @@ public class MainFragment extends Fragment {
                 double currentBalance = 0.00;
                 for (DataSnapshot postSnapshot1 : dataSnapshot.getChildren()) {
                     MoneyTransaction t = postSnapshot1.getValue(MoneyTransaction.class);
+                    t.setKey(postSnapshot1.getKey());
                     transactions.add(t);
+                    //assert t != null;
                     if (t.getTransactionType() == 0) {
                         currentBalance -= t.getAmount();
                     }
@@ -98,37 +126,25 @@ public class MainFragment extends Fragment {
                 Collections.sort(transactions);
 
                 // Populate the listview
-                ListViewAdapter adapter = new ListViewAdapter(getActivity(), transactions);
+                adapter = new ListViewAdapter(mContext, transactions);
                 simpleList.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
+
+                /*
+                binding.simpleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+                        TextView v = view.findViewById(R.id.textViewLV4);
+                        Log.d("click", "clicked");
+                    }
+                });
+                 */
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-
-        return view;
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainFragmentDirections.ActionMainToAddFunds action = MainFragmentDirections.actionMainToAddFunds(0);
-                NavHostFragment.findNavController(MainFragment.this).navigate(action);
-            }
-        });
-
-        view.findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainFragmentDirections.ActionMainToDeduction action = MainFragmentDirections.actionMainToDeduction(0);
-                NavHostFragment.findNavController(MainFragment.this).navigate(action);
             }
         });
     }
