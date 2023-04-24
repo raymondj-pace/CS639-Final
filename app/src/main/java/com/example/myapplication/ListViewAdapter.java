9d93629c9f5fe66accf5c7ff0ce1d93c6475ed99
Raymond Jennings <ray@Rays-15-home-purple-macbook-pro-2.local>
2023-04-24 18:14:26 -0400

    Add colored $ indicators and X button to delete rows

--- app/src/main/java/com/example/myapplication/ListViewAdapter.java
+++ app/src/main/java/com/example/myapplication/ListViewAdapter.java
@@ -1,16 +1,17 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.Navigation;
 
 import java.text.DecimalFormat;
@@ -19,11 +20,13 @@ import java.util.ArrayList;
 public class ListViewAdapter extends ArrayAdapter<MoneyTransaction> {
 
     private ArrayList<MoneyTransaction> transactionList;
+    private Context mContext;
 
     public ListViewAdapter(Context context, ArrayList<MoneyTransaction> transactionList) {
         super(context, 0, transactionList);
 
         this.transactionList = transactionList;
+        this.mContext = context;
     }
 
     @SuppressWarnings("unused")
@@ -55,12 +58,14 @@ public class ListViewAdapter extends ArrayAdapter<MoneyTransaction> {
         TextView textViewLV2 = convertView.findViewById(R.id.textViewLV2);
         TextView textViewLV3 = convertView.findViewById(R.id.textViewLV3);
         TextView textViewLV4 = convertView.findViewById(R.id.textViewLV4);
+        TextView textViewLV5 = convertView.findViewById(R.id.textViewLV5);
 
+        textViewLV1.setText("$");
         if (currentItem.getTransactionType() == 0) {
-            textViewLV1.setText("-");
+            textViewLV1.setTextColor(Color.parseColor("#FFEE4B2B"));
         }
         else {
-            textViewLV1.setText("+");
+            textViewLV1.setTextColor(Color.parseColor("#FF2DB83D"));
         }
 
         textViewLV2.setText(currentItem.getDate());
@@ -72,6 +77,7 @@ public class ListViewAdapter extends ArrayAdapter<MoneyTransaction> {
 
         textViewLV4.setText(currentItem.getDescription());
 
+        textViewLV5.setText("X");
 
 
         textViewLV1.setOnClickListener(new View.OnClickListener() {
@@ -119,13 +125,9 @@ public class ListViewAdapter extends ArrayAdapter<MoneyTransaction> {
             }
         });
 
-        textViewLV4.setOnLongClickListener(new View.OnLongClickListener() {
+        textViewLV4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-
-                Log.d("LONG CLICK", "LONG CLICKED");
-                remove(position);
-                /*
+            public void onClick(View v) {
                 Bundle bundle = new Bundle();
                 bundle.putParcelable("item", currentItem);
 
@@ -135,9 +137,21 @@ public class ListViewAdapter extends ArrayAdapter<MoneyTransaction> {
                 else if (currentItem.getTransactionType() == 0) {
                     Navigation.findNavController(v).navigate(R.id.action_Main_to_Deduction, bundle);
                 }
-                */
+            }
+        });
 
-                return false;
+        textViewLV5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(mContext)
+                        .setTitle("Confirm")
+                        .setMessage("Do you really want to delete?")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                remove(position);
+                            }})
+                        .setNegativeButton(android.R.string.no, null).show();
             }
         });
 
